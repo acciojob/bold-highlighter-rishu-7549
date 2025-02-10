174a6ff1efe751f4697ce26721da600e89c245b0
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-10 21:58:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	let str = document.querySelectorAll('strong');
+	str.forEach(ele => {
+		ele.style.color = 'green';
+	});
 }
 
 
 function return_normal() {
     //Write your code here
-
+let str = document.querySelectorAll('strong');
+	str.forEach(ele => {
+		ele.style.color = 'black';
     
+});
 }
